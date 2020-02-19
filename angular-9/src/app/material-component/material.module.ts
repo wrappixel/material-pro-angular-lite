720faa2308cd39922e570356a9e1e0ce30ff5e95
Nirav joshi <niravjoshi87@gmail.com>
2020-02-19 20:21:19 +0530

    'updated-to-angular9'

---
+++ angular-9/src/app/material-component/material.module.ts
@@ -0,0 +1,68 @@
+import 'hammerjs';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { DemoMaterialModule } from '../demo-material-module';
+import { CdkTableModule } from '@angular/cdk/table';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialRoutes } from './material.routing';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+import { GridComponent } from './grid/grid.component';
+import { ListsComponent } from './lists/lists.component';
+import { MenuComponent } from './menu/menu.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { ExpansionComponent } from './expansion/expansion.component';
+import { ChipsComponent } from './chips/chips.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ProgressSnipperComponent } from './progress-snipper/progress-snipper.component';
+import { ProgressComponent } from './progress/progress.component';
+import {
+  DialogComponent,
+  DialogOverviewExampleDialogComponent
+} from './dialog/dialog.component';
+import { TooltipComponent } from './tooltip/tooltip.component';
+import { SnackbarComponent } from './snackbar/snackbar.component';
+import { SliderComponent } from './slider/slider.component';
+import { SlideToggleComponent } from './slide-toggle/slide-toggle.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(MaterialRoutes),
+    DemoMaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    CdkTableModule
+  ],
+  providers: [],
+  entryComponents: [DialogOverviewExampleDialogComponent],
+  declarations: [
+    ButtonsComponent,
+    GridComponent,
+    ListsComponent,
+    MenuComponent,
+    TabsComponent,
+    StepperComponent,
+    ExpansionComponent,
+    ChipsComponent,
+    ToolbarComponent,
+    ProgressSnipperComponent,
+    ProgressComponent,
+    DialogComponent,
+    DialogOverviewExampleDialogComponent,
+    TooltipComponent,
+    SnackbarComponent,
+    SliderComponent,
+    SlideToggleComponent
+  ]
+})
+export class MaterialComponentsModule {}
