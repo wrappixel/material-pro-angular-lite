058d602d21c5770c1c692b6346be540e2b326f0d
wrappixel <info@wrappixel.com>
2022-05-25 22:33:03 +0530

    ''

--- angular-10/src/app/dashboard/dashboard.routing.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { DashboardComponent } from './dashboard.component';
-
-export const DashboardRoutes: Routes = [{
-  path: '',
-  component: DashboardComponent
-}];
--- angular-7/src/app/starter/starter.routing.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { StarterComponent } from './starter.component';
-
-export const StarterRoutes: Routes = [{
-  path: '',
-  component: StarterComponent
-}];
